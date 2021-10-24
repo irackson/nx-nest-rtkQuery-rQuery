634a0354ff625ee677ec7c0d85807d86bbfb2fa2
irackson <nasirus@protonmail.com>
2021-10-24 15:34:39 -0400

    next-api complete

--- apps/nest-server/src/app/app.controller.ts
+++ apps/nest-server/src/app/app.controller.ts
@@ -1,13 +1,23 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+    constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getData() {
-    return this.appService.getData();
-  }
+    @Get()
+    getData() {
+        return this.appService.getData();
+    }
+
+    @Post() // POST http://localhost:3333/api
+    create(@Body() { text }: { text: string }) {
+        return this.appService.add(text);
+    }
+
+    @Post('setDone') // POST http://localhost:3333/api/setDone
+    setDone(@Body() { id, done }: { id: number; done: boolean }) {
+        return this.appService.setDone(id, done);
+    }
 }
--- apps/nest-server/src/app/app.service.ts
+++ apps/nest-server/src/app/app.service.ts
@@ -1,8 +1,30 @@
 import { Injectable } from '@nestjs/common';
 
+interface Todo {
+    id: number;
+    text: string;
+    done: boolean;
+}
+
 @Injectable()
 export class AppService {
-  getData(): { message: string } {
-    return { message: 'Welcome to nest-server!' };
-  }
+    private todos: Todo[] = [];
+
+    getData(): Todo[] {
+        return this.todos;
+    }
+
+    add(text: string) {
+        this.todos.push({
+            id: this.todos.length,
+            text,
+            done: false,
+        });
+    }
+
+    setDone(id: number, done: boolean) {
+        this.todos = this.todos.map((todo) =>
+            todo.id === id ? { ...todo, done } : todo
+        );
+    }
 }
