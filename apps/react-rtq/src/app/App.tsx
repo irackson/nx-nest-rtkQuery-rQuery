e89ac4e4e4a9e59a984f7088a0c8f5e53d58dde6
irackson <nasirus@protonmail.com>
2021-10-28 19:44:57 -0400

    getAll rtq

--- apps/react-rtq/src/app/App.tsx
+++ apps/react-rtq/src/app/App.tsx
@@ -1,10 +1,38 @@
+import { ApiProvider } from '@reduxjs/toolkit/dist/query/react';
+import { todoApi } from '../store';
 import './App.css';
+import type { Todo } from '@nest-todos/shared-types';
+import { Fragment } from 'react';
 
-export function App() {
+export function TodoApp() {
+    const { data: todos } = todoApi.useGetAllQuery();
+
     return (
-        <div className="app">
-            <h1>hello</h1>
+        <div className="App">
+            <div className="todos">
+                {todos?.map((todo) => (
+                    <Fragment key={todo.id}>
+                        <div>
+                            <input
+                                type="checkbox"
+                                checked={todo.done}
+                                onChange={() => {}}
+                            />
+                            <span>{todo.text}</span>
+                        </div>
+                        <button onClick={() => {}}>Delete</button>
+                    </Fragment>
+                ))}
+            </div>
         </div>
+    );
+}
+
+function App() {
+    return (
+        <ApiProvider api={todoApi}>
+            <TodoApp />
+        </ApiProvider>
     );
 }
 
--- apps/react-rtq/src/store.ts
+++ apps/react-rtq/src/store.ts
@@ -1,2 +1,14 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import type { Todo } from '@nest-todos/shared-types';
+
+export const todoApi = createApi({
+    reducerPath: 'todoApi',
+    baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3333/api' }),
+    tagTypes: ['Todos'],
+    endpoints: (builder) => ({
+        getAll: builder.query<Todo[], void>({
+            query: () => `/`,
+            providesTags: [{ type: 'Todos', id: 'LIST' }],
+        }),
+    }),
+});
